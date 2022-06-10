07be5c94fd241552857ba20103e0ab01fba6e93d
Emerson Floriano <florianoe574@gmail.com>
2022-06-10 11:20:15 -0300

    linked list

--- linked_list/crud.c
+++ linked_list/crud.c
@@ -2,17 +2,25 @@
 #include<stdlib.h>
 
 typedef struct node Node;
+typedef struct list List;
 
 struct node{
   int data;
   Node *next;
 };
 
-Node* create(){
-  return NULL;
+struct list{
+  Node *head;
+};
+
+List* create(void){
+  List *list = (List *) malloc(sizeof(List));
+  list -> head = NULL;
+
+  return list;
 }
 
-Node* insert(Node *list, int data){
+void insert_begin(List *list, int data){
   Node *temp = (Node *) malloc(sizeof(Node));
 
   if(temp == NULL){
@@ -21,32 +29,98 @@ Node* insert(Node *list, int data){
   }
 
   temp -> data = data;
-  temp -> next = list;
+  temp -> next = list -> head;
 
-  return temp;
+  list -> head = temp;
 }
 
-void read(Node* list){
-
-  Node *invert = create();
-
-  for(Node *i = list; i != NULL; i = i -> next){
-    // printf("%d ", i -> data);
-    invert = insert(invert, i -> data);
+void insert_end(List *list, int data){
+  Node *end = NULL;
+  for(Node* i = list -> head; i != NULL; i = i -> next){
+    if(i -> next == NULL){
+      end = i;
+      break;
+    }
   }
-  printf("\n");
-  for(Node *i = invert; i != NULL; i = i -> next){
+
+  Node* temp = (Node *) malloc(sizeof(Node));
+  temp -> data = data; 
+  temp -> next = NULL;
+  end -> next = temp;
+}
+
+void read(List *list){
+  printf("\n[ ");
+
+  for(Node *i = list -> head; i != NULL; i = i -> next){
     printf("%d ", i -> data);
   }
-  printf("\n");
+
+  printf("]\n");
+}
+
+void delete_first(List *list){
+  Node *temp = list -> head;
+  list -> head = temp -> next;
+  free(temp);
 }
 
 int main(){
-  Node *list = create();
-  
-  list = insert(list, 5);
-  list = insert(list, 12);
-  
-  read(list);
+  int op = 0, created = 0;
+  List *list = NULL;
+
+  while(op != 9){
+    printf("\n1 - Criar"
+      "\n2 - Imprimir"
+      "\n3 - Inserir no inicio"
+      "\n4 - Inserir no final"
+      "\n5 - Deletar do inicio"
+      "\n9 - Sair"
+    );
+    printf("\n\nInforme a opcao: ");
+    scanf("%d", &op);
+
+    if(op == 9) break;
+    if(op == 1){
+      list = create();
+      created = 1;
+    }
+    else if(!created){
+      printf(
+        "\n-- A lista deve ser criada "
+        "antes de qualquer operacao! --\n"
+      );
+    }
+    else{
+        if(op == 2){
+        read(list);
+      }
+      if(op == 3){
+        int data = 0;
+        printf("\nInforme o valor: ");
+        scanf("%d", &data);
+        insert_begin(list, data);
+      }
+      if(op == 4){
+        int data = 0;
+        printf("\nInforme o valor: ");
+        scanf("%d", &data);
+        insert_end(list, data);
+      }
+      if(op == 5){
+        delete_first(list);
+      }
+      if(op == 6){
+
+      }
+      if(op == 7){
+        
+      }
+      if(op == 8){
+        
+      }
+    }
+    
+  }
 }
 
